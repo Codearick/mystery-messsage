c4d13385f30f8b232099c669c7491e95ca84e9be
Codearick <rohan.gautam650@gmail.com>
2026-02-01 20:07:58 +0545

    api for deleting message

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,9 @@
+// import UserModel from "@/model/User.model";
+// import dbConnect from "@/lib/dbConnect";
+// import { User } from "next-auth";
+
+// export async function DELETE(request: Request){
+//     await dbConnect();
+
+//     const {}
+// }
